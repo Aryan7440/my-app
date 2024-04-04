7ffb2ca58be645d45362327fb8cd2ebe19b74d25
Aryan7440 <aryanshukla744@gmail.com>
2024-04-04 16:48:26 +0530

    third commit

--- src/Form/Form.jsx
+++ src/Form/Form.jsx
@@ -10,110 +10,140 @@ const Form = () => {
       setStep((prevStep) => (prevStep + n) % 7)
     }
   }
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    // Handle form submission here
+  }
   return (
     <div>
-      <form id="regForm" action="">
-        <h1>Register:</h1>
-        <div className="tab" style={{ display: step === 0 ? 'block' : 'none' }}>
-          Name:
-          <p>
-            <input placeholder="First name..." />
-          </p>
-          <p>
-            <input placeholder="Last name..." />
-          </p>
-        </div>
+      <form id="regForm" action="" onSubmit={handleSubmit}>
+        <h1>Register</h1>
+        <div className="tab-section">
+          <div
+            className="tab"
+            style={{ display: step === 0 ? 'block' : 'none' }}
+          >
+            Name:
+            <p>
+              <input placeholder="First name..." />
+            </p>
+            <p>
+              <input placeholder="Last name..." />
+            </p>
+          </div>
 
-        <div className="tab" style={{ display: step === 1 ? 'block' : 'none' }}>
-          Contact Info:
-          <p>
-            <input
-              placeholder="E-mail..."
-              onChange={() => (this.className = '')}
-            />
-          </p>
-          <p>
-            <input
-              placeholder="Phone..."
-              onChange={() => (this.className = '')}
-            />
-          </p>
-        </div>
+          <div
+            className="tab"
+            style={{ display: step === 1 ? 'block' : 'none' }}
+          >
+            Contact Info:
+            <p>
+              <input
+                placeholder="E-mail..."
+                onChange={() => (this.className = '')}
+              />
+            </p>
+            <p>
+              <input
+                placeholder="Phone..."
+                onChange={() => (this.className = '')}
+              />
+            </p>
+          </div>
 
-        <div className="tab" style={{ display: step === 2 ? 'block' : 'none' }}>
-          Birthday:
-          <p>
-            <input placeholder="dd" onChange={() => (this.className = '')} />
-          </p>
-          <p>
-            <input placeholder="mm" onChange={() => (this.className = '')} />
-          </p>
-          <p>
-            <input placeholder="yyyy" onChange={() => (this.className = '')} />
-          </p>
-        </div>
+          <div
+            className="tab"
+            style={{ display: step === 2 ? 'block' : 'none' }}
+          >
+            Birthday:
+            <p>
+              <input placeholder="dd" onChange={() => (this.className = '')} />
+            </p>
+            <p>
+              <input placeholder="mm" onChange={() => (this.className = '')} />
+            </p>
+            <p>
+              <input
+                placeholder="yyyy"
+                onChange={() => (this.className = '')}
+              />
+            </p>
+          </div>
 
-        <div className="tab" style={{ display: step === 3 ? 'block' : 'none' }}>
-          Login Info:
-          <p>
-            <input
-              placeholder="Username..."
-              onChange={() => (this.className = '')}
-            />
-          </p>
-          <p>
-            <input
-              placeholder="Password..."
-              onChange={() => (this.className = '')}
-            />
-          </p>
-        </div>
-        <div className="tab" style={{ display: step === 4 ? 'block' : 'none' }}>
-          Explain your startup idea
-          <p>
-            <input
-              placeholder="Idea..."
-              onChange={() => (this.className = '')}
-            />
-          </p>
-        </div>
-        <div className="tab" style={{ display: step === 5 ? 'block' : 'none' }}>
-          What kind of a person you are
-          <p>
-            <input
-              placeholder="Tell us about yourself..."
-              onChange={() => (this.className = '')}
-            />
-          </p>
-        </div>
-        <div className="tab" style={{ display: step === 6 ? 'block' : 'none' }}>
-          What are your expectations?
-          <p>
-            <input
-              placeholder="Expectations..."
-              onChange={() => (this.className = '')}
-            />
-          </p>
-        </div>
-        <div className="btn-section">
-          <div style={{ overflow: 'auto' }}>
-            <div style={{ float: 'right' }}>
-              <button type="button" id="prevBtn" onClick={() => nextPrev(-1)}>
-                Previous
-              </button>
-              <button type="button" id="nextBtn" onClick={() => nextPrev(1)}>
-                Next
-              </button>
-            </div>
+          <div
+            className="tab"
+            style={{ display: step === 3 ? 'block' : 'none' }}
+          >
+            Login Info:
+            <p>
+              <input
+                placeholder="Username..."
+                onChange={() => (this.className = '')}
+              />
+            </p>
+            <p>
+              <input
+                placeholder="Password..."
+                onChange={() => (this.className = '')}
+              />
+            </p>
+          </div>
+          <div
+            className="tab"
+            style={{ display: step === 4 ? 'block' : 'none' }}
+          >
+            Explain your startup idea
+            <p>
+              <input
+                placeholder="Idea..."
+                onChange={() => (this.className = '')}
+              />
+            </p>
+          </div>
+          <div
+            className="tab"
+            style={{ display: step === 5 ? 'block' : 'none' }}
+          >
+            What kind of a person you are
+            <p>
+              <input
+                placeholder="Tell us about yourself..."
+                onChange={() => (this.className = '')}
+              />
+            </p>
+          </div>
+          <div
+            className="tab"
+            style={{ display: step === 6 ? 'block' : 'none' }}
+          >
+            What are your expectations?
+            <p>
+              <input
+                placeholder="Expectations..."
+                onChange={() => (this.className = '')}
+              />
+            </p>
           </div>
         </div>
-
-        {/* <div style={{ textAlign: 'center', marginTop: '40px' }}>
-          <span className="step"></span>
-          <span className="step"></span>
-          <span className="step"></span>
-          <span className="step"></span>
-        </div> */}
+        <div className="btn-section">
+          <div className="lr">
+            <button type="button" id="prevBtn" onClick={() => nextPrev(-1)}>
+              Previous
+            </button>
+            <button type="button" id="nextBtn" onClick={() => nextPrev(1)}>
+              Next
+            </button>
+          </div>
+          <div className="submit-area">
+            <button
+              className="btn-submit"
+              style={{ display: step === 6 ? 'block' : 'none' }}
+              type="submit"
+            >
+              Submit
+            </button>
+          </div>
+        </div>
       </form>
     </div>
   )
